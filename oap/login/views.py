874fe81b90c5c326678bdfaf04390f7369c67c84
mayank dixit <44468368+mayank0802@users.noreply.github.com>
2019-05-19 16:24:24 +0530

    Update views.py

--- oap/login/views.py
+++ oap/login/views.py
@@ -4,11 +4,14 @@ from .models import Customer
 
 def index(request):
     return render(request, 'login/index.html')
-def register(request):
-    return render(request, 'login/register.html')
+def register(request):    
     if request.method =="POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
         user_name = request.POST.get('user_name','')
         password = request.POST.get('password','')
-        customer = Customer(user_name = user_name, password = password)
+        customer = Customer(first_name= first_name, last_name= last_name, user_name = user_name, password = password)
         customer.save()
-    
+    return render(request, 'login/register.html')        
+def home(request):
+    return render(request, 'login/home.html')    
