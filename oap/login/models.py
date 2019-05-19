017ffd46a8a956134dae428ed4cc7b529d85d7fe
mayank dixit <44468368+mayank0802@users.noreply.github.com>
2019-05-19 16:25:26 +0530

    Update models.py

--- oap/login/models.py
+++ oap/login/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     user_id = models.AutoField
-    user_name = models.CharField(max_length = 50)
-    password = models.CharField(max_length = 59)
+    first_name = models.CharField(max_length = 50, default ="")
+    last_name = models.CharField(max_length = 50, default ="", null = 'False')
+    user_name = models.CharField(max_length = 50, default ="")
+    password = models.CharField(max_length = 59,default="")
 
- 
+def __str__(self):
+    return self.user_name
